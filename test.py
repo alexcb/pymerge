551efc41b8f5975bc605038dc99e6854758f5405
Alex Couture-Beil <alex@earthly.dev>
2021-07-05 13:06:13 -0700

    again!

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ import timeit
 
 a = list(range(-1, 1700))
 b = list(range(1400, 1800))
-def merge1():
+def merge_test():
    m1 = merge.merge(a, b)
-def merge2():
+def sort_test():
    m2 = list(a + b).sort()
 
-sort_time = timeit.timeit("merge1()", setup="from __main__ import merge1", number=100000)
-merge_time = timeit.timeit("merge2()", setup="from __main__ import merge2",number=100000)
+sort_time = timeit.timeit("sort_test()", setup="from __main__ import sort_test", number=100000)
+merge_time = timeit.timeit("merge_test()", setup="from __main__ import merge_test",number=100000)
 
 print(f'sort took {sort_time} seconds')
 print(f'merge took {merge_time} seconds')
