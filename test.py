727a95865ea32967547619eb54341351f32f11b5
Alex Couture-Beil <alex@earthly.dev>
2021-07-05 13:02:18 -0700

    use -O3 optimization
    Signed-off-by: Alex Couture-Beil <alex@mofo.ca>

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 import merge
+import timeit
 
-a = [-1,3.14,10]
-b = [2,8,9,100]
+a = list(range(-1, 1700))
+b = list(range(1400, 1800))
+def merge1():
+   m1 = merge.merge(a, b)
+def merge2():
+   m2 = list(a + b).sort()
 
-print(f'list a is {a}')
-print(f'list b is {b}')
-c = merge.merge(a, b)
-print(f'merged list is {c}')
+sort_time = timeit.timeit("merge1()", setup="from __main__ import merge1", number=100000)
+merge_time = timeit.timeit("merge2()", setup="from __main__ import merge2",number=100000)
+
+print(f'sort took {sort_time} seconds')
+print(f'merge took {merge_time} seconds')
